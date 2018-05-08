22e001820c91dcc2bc6836caf40892aa57a0792c
LahBah <lahbahm@gmail.com>
2018-05-08 07:16:32 +0400

    del

--- firstJsHomework/1.2 functionFind.js
+++
@@ -1,8 +0,0 @@
-function find(arr, str){
-	var mapped = arr.map(function(element){
-		return element.toLowerCase();
-	});
-		return(mapped.indexOf(str) != -1);
-}
-alert(find(['Пётр', 'Василий', 'Мария', 'Екатерина', 'Logan', 'Jack', 'Isabella', 'Elizabeth'], 'elizabeth'));
-           
